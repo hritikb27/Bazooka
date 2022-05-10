454e7fa949f6fb24d23437f3e0b0c8ea673a3f36
unknown <hritikb90@gmail.com>
2022-05-10 22:42:22 +0530

    added browse battles skeleton page

--- bazooka-frontend/components/Layout.js
+++ bazooka-frontend/components/Layout.js
@@ -7,7 +7,7 @@ export default function Layout({children}) {
                 <ul className="mt-[10rem] font-semibold text-xl">
                     <Link href="/createbattle"><li className="border-y border-black w-[100%] p-[1rem] cursor-pointer">Create a Battle</li></Link>
                     <Link href="/InitializedBattles"><li className="border-y border-black w-[100%] p-[1rem] cursor-pointer">Initialized Battles</li></Link>
-                    <li></li>
+                    <Link href="/browseBattles"><li className="border-y border-black w-[100%] p-[1rem] cursor-pointer">Browse Battles</li></Link>
                     <li></li>
                 </ul>
             </aside>
---
+++ bazooka-frontend/pages/browseBattles.js
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import { useMoralis, useWeb3ExecuteFunction } from "react-moralis";
+import ABI from '../utils/abi.json'
+
+export default function BrowseBattles() {
+    const { Moralis } = useMoralis();
+    const contractProcessor = useWeb3ExecuteFunction();
+    const [battles, setBattles] = useState([]);
+
+    useEffect(()=>{
+        let battleNum;
+        async function getBattleData() {
+            const options = {
+                contractAddress: "0x0F6d227e58314Af97a11a29fACb7B96bFE3d0602",
+                functionName: "getBattleAmount",
+                abi: ABI,
+            }
+    
+            const data = await Moralis.executeFunction(options)
+            battleNum = parseInt(data);
+        }
+
+        async function getAllBattles(){
+            const options = {
+                contractAddress: "0x0F6d227e58314Af97a11a29fACb7B96bFE3d0602",
+                functionName: "getBattleData",
+                abi: ABI,
+            }
+            
+            for(let i=1; i<=2; i++){
+                console.log(i)
+                const data = await contractProcessor.fetch({params: {...options, params:{battleId:`${i}`}}})
+                setBattles(prev=>[...prev,data]);
+            }
+        }
+
+
+        getBattleData();
+        getAllBattles();
+
+    },[])
+
+    function getBattles(){
+        console.log(battles)
+    }
+
+    return(
+        <div className="flex flex-col items-center justify-center mt-[5rem] gap-5">
+            <button onClick={getBattles}>Click</button>
+            <ul className="w-[100%] md:w-[80%] lg:w-[60%] xl:w-[60%] m-auto flex gap-5 flex-wrap">
+                {battles.map(battle=>{
+                    if(!battle.finalized) return;
+                    return <li className="w-[30%] h-[300px]  border border-black rounded flex flex-col justify-between cursor-pointer">
+                        <img src={battle[0][1]} className="min-h-[210px] max-h-[210px] min-w-[256px] " />
+                        <p>Amount: {parseInt(battle.amount)}</p>
+                        <button className="border border-white bg-black text-white h-[18%] ">Battle</button>
+                    </li>
+                })}
+            </ul>
+        </div>
+    )
+}
